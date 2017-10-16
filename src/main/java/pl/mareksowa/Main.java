7dfa69230a3d024b15d5e78fd7e055395f489756
Kerawos <Maresowa@gmail.com>
2017-10-16 20:35:23 +0200

    redame md2

--- src/main/java/pl/mareksowa/Main.java
+++ src/main/java/pl/mareksowa/Main.java
@@ -6,6 +6,6 @@ public class Main {
     public static void main(String[] args) {
         // write your code here
         //sample comment
-        System.out.println("test");
+        System.out.println("hello world");
     }
 }
