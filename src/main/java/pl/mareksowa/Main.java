7ab0e287f9e21bfcebf25bd9b340d6220920cd28
Kerawos <Maresowa@gmail.com>
2017-10-16 20:26:58 +0200

    git commit3

---
+++ src/main/java/pl/mareksowa/Main.java
@@ -0,0 +1,11 @@
+package pl.mareksowa;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        //sample comment
+        System.out.println("test");
+    }
+}
