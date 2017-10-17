16288ba9e839293703359f86dbd5b6d9469e834c
Kerawos <Maresowa@gmail.com>
2017-10-17 13:33:48 +0200

    Add 2 simple tests (shellCommand.statistics() , shellProgram.resetPrompt())

--- src/test/java/pl/mareksowa/models/ShellCommandTest.java
+++ src/test/java/pl/mareksowa/models/ShellCommandTest.java
@@ -23,13 +23,24 @@ public class ShellCommandTest {
 
     @Test
     public void testStatistics(){
-//        ShellProgram program = new ShellProgram();
-//        assertEquals("cd:0:0\n" +
-//                "tree:0:0\n" +
-//                "dir:0:0\n" +
-//                "prompt:0:0\n" +
-//                "statistics:1:0", command.statistics());
+        //ShellProgram shellProgram = new ShellProgram();
+        assertEquals("cd:0:0\n" +
+                "tree:0:0\n" +
+                "dir:0:0\n" +
+                "prompt:0:0\n" +
+                "statistics:1:0\n", command.statistics());
     }
+
+    @Test
+    public void testPromptReset(){
+        ShellProgram shellProgram = new ShellProgram();
+        shellProgram.setIsPromptDirectory(true);
+        shellProgram.setPrompt("prompt test");
+        shellProgram.promptReset();
+        assertEquals(false, shellProgram.isIsPromptDirectory());
+        assertEquals("$", shellProgram.getPrompt());
+    }
+
 
 
 
